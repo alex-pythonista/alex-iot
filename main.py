0343b5154484097ce8e1e26f0b2c7a7fb82b223a
alex-pythonista <indrajit.alex@gmail.com>
2021-01-27 14:04:01 +0600

    final commit. Alexa is ready!

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import speech_recognition as sr
 import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+
 
 listener = sr.Recognizer()
 engine = pyttsx3.init()
@@ -18,17 +23,36 @@ def take_command():
             command = listener.recognize_google(voice)
             command = command.lower()
             if 'alexa' in command:
-                command = command.replace('play', '')
-                talk(command)
-
+                command = command.replace('alexa', '')
+                print(command)
     except:
         pass
     return command
 
 def run_alexa():
     command = take_command()
+    print(command)
     if 'play' in command:
-        talk('playing...')
-        print('playing')
+        song = command.replace('play', '')
+        talk('playing '+ song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        print(time)
+        talk('Current time is '+ time)
+    elif 'who the heck is' in command:
+        person = command.replace('who the heck is', '')
+        info = wikipedia.summary(person, 2)
+        print(info)
+        talk(info)
+    elif 'date' in command:
+        talk('sorry, I have a headache')
+    elif 'are you single' in command:
+        talk('I am in a relationship with wifi')
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+    else:
+        talk('Please say the command again.')
 
-run_alexa()
+while True:
+    run_alexa()
